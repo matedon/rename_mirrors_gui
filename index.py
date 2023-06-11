68ce13d954e0c795e342a257d557f49cefb615b7
matedon <matedon@gmail.com>
2023-06-11 09:50:57 +0200

    clone explorer

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ import json
 import os
 import shutil
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QLineEdit
 from pathlib import Path
 import json
 
@@ -12,45 +12,96 @@ if os.path.isfile('./config/config.json') == False:
 with open('./config/config.json', 'r') as f:
     config = json.load(f)
 
-qt_creator_file = "renamemirrors.ui"
-Ui_MainWindow, QtBaseClass = uic.loadUiType(qt_creator_file)
-
 class MirrorModel(QtCore.QAbstractListModel):
     def __init__(self, *args, todos=None, **kwargs):
         super(MirrorModel, self).__init__(*args, **kwargs)
 
-class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    clNum = 0
+    paths = []
+    lists = []
+
     def __init__(self):
-        QtWidgets.QMainWindow.__init__(self)
-        Ui_MainWindow.__init__(self)
-        self.setupUi(self)
+        super(MainWindow, self).__init__()
+        uic.loadUi('renamemirrors.ui', self)
         self.model = MirrorModel()
         self.load()
-        #self.addButton.pressed.connect(self.list1)
-        self.path1.returnPressed.connect(self.list1fill)
+
+    def duplicateLayout(self):
+        self.clNum = self.clNum + 1
+        layout2 = QVBoxLayout()
+        #cwName = self.layoutv1.objectName() + '__c' + str(self.clNum)
+        #self.layouth1.addLayout(self.layout2, objectName = cwName)
+        self.layouth1.addLayout(layout2)
+
+        # Iterate over widgets in layoutv1
+        for index in range(self.layoutv1.count()):
+            widget = self.layoutv1.itemAt(index).widget()
+            if widget is not None:
+                cwName = widget.objectName() + '__' + str(self.clNum)
+                cw = type(widget)(objectName = cwName)
+                layout2.addWidget(cw)
+                cw.installEventFilter(self)
+                if "path" in cw.objectName():
+                    self.paths.append(cw)
+                    # TODO: DOUBLECLICK NOT WORKS!
+                    cw.itemDoubleClicked = self.listDoubleClick
+                if "list" in cw.objectName():
+                    self.lists.append(cw)
+
+    def eventFilter(self, obj, event):
+        #print('eventFilter', event.type(), obj.objectName())
+        if event.type() == QtCore.QEvent.KeyPress and "path" in obj.objectName():
+            if event.key() in (QtCore.Qt.Key_Return, QtCore.Qt.Key_Enter):
+                self.listFill(obj.text(), int(obj.objectName().split('__')[1]))
+                return True
+        if event.type() == QtCore.QEvent.MouseButtonPress:
+            self.single_click_timer.start()
+            return True
+        elif event.type() == QtCore.QEvent.MouseButtonDblClick:
+            self.single_click_timer.stop()
+            print('double click')
+            return True
+        return super(MainWindow, self).eventFilter(obj, event)
 
     def load(self):
-        self.list1fill(config['paths'][0])
-        self.list1.itemDoubleClicked.connect(self.listDoubleClick)
+        self.single_click_timer = QtCore.QTimer()
+        self.single_click_timer.setInterval(200)
+        self.single_click_timer.timeout.connect(self.single_click)
 
-    def list1fill(self, path=None):
-        if (path is None):
-            path = self.path1.text()
-        self.path1.setText(path)
-        directory = Path(path)
-        #directories = [entry for entry in directory.iterdir() if entry.is_dir()]
+        self.paths.append(self.path1)
+        self.lists.append(self.list1)
+        self.listFill(config['paths'][0])
+        self.list1.itemDoubleClicked.connect(self.listDoubleClick)
+        #self.addButton.pressed.connect(self.list1)
+        self.path1.returnPressed.connect(self.listFill)
+        self.dev1.clicked.connect(self.duplicateLayout)
+
+    def single_click(self):
+        self.single_click_timer.stop()
+        print('timeout, must be single click')
+
+    def listFill(self, route=None, num=0):
+        path = self.paths[num]
+        list = self.lists[num]
+        if (route is None):
+            route = path.text()
+        path.setText(route)
+        directory = Path(route)
         directories = [entry for entry in directory.iterdir()]
-        self.list1.clear()
-        self.list1.addItem('[. .]')
+        list.clear()
+        list.addItem('[. .]')
         for dir_name in directories:
             if (dir_name.is_dir()):
                 name = '[' + dir_name.name + ']'
             else:
                 name = dir_name.name
-            self.list1.addItem(name)
+            list.addItem(name)
 
     def listDoubleClick(self, item):
-        path = self.path1.text().strip(os.sep)
+        path = self.path1.text().rstrip(os.sep)
         itemText = item.text()
         isBack = False
         if (itemText == '[. .]'):
@@ -58,14 +109,14 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         isDir = False
         if (itemText[0] == '['):
             isDir = True
-            itemText = itemText.strip('[').strip(']')
+            itemText = itemText.lstrip('[').rstrip(']')
         if (isBack):
             path = os.path.dirname(path)
         else:
             path = path + os.sep + itemText
         print(path)
         if isDir:
-            self.list1fill(path)
+            self.listFill(path)
 
 
     
